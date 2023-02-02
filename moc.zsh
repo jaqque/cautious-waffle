599cd880cbb51425a5127e14efa5224e8811b654
John H. Robinson, IV <jhriv@ucla.edu>
2023-02-01 20:21:59 -0800

    DRYer
    - combined stars and gems arrays
    - first element of gems is the stars to display
    - removed subshells from calling functions
      - $RANDOM was inheriting its seed from the invoking shell
      - since the invoking shell never "called" $RANDOM, the seed never
        changed
    - still the same, crappy, output
    
    TODO:
    - eh, done?
    - adjust output?
    - add in rest of gem names?
    - set the actual chance for selecting a type?
    - outer loop for finding that ultra super mega rare 5 * * * * * gem?
    
    - nah, i set out to do what i wanted. only need to adjust output as
      apporpriate
    - might need a second outer loop for *MOAR RUNS*

--- moc.zsh
+++ moc.zsh
@@ -1,51 +1,73 @@
 #!/usr/bin/env zsh
 
 typeset -A gems
-gems[1]='trickshot black-rose everlasting-torment'
-gems[2]='power-and-command hunger bloody-reach'
-gems[2/5]='bottled-hope phoenix-ashes bsj'
-gems[3/5]=$gems[2/5]
-gems[4/5]=$gems[2/5]
-gems[5/5]=$gems[2/5]
+gems[1]='\u2605 trickshot black-rose everlasting-torment'
+gems[2]='\u2605\u20\u2605 power-and-command hunger bloody-reach'
+gems[2/5]='\u2605\u20\u2605\u20\u2606\u20\u2606\u20\u2606 bottled-hope phoenix-ashes bsj'
+gems[3/5]='\u2605\u20\u2605\u20\u2605\u20\u2606\u20\u2606 '${${(ps: :)gems[2/5]}[2,-1]}
+gems[4/5]='\u2605\u20\u2605\u20\u2606\u20\u2605\u20\u2606 '$gems[2/5]
+gems[5/5]='\u2605\u20\u2605\u20\u2606\u20\u2606\u20\u2605 '$gems[2/5]
 
-typeset -A stars
-stars[1]=$'\u2605'
-stars[2]=$'\u2605\u20\u2605'
-stars[2/5]=$'\u2605\u20\u2605\u20\u2606\u20\u2606\u20\u2606'
-stars[3/5]=$'\u2605\u20\u2605\u20\u2605\u20\u2606\u20\u2606'
-stars[4/5]=$'\u2605\u20\u2605\u20\u2605\u20\u2605\u20\u2606'
-stars[5/5]=$'\u2605\u20\u2605\u20\u2605\u20\u2605\u20\u2605'
+# typeset -A stars
+# stars[1]=$'\u2605'
+# stars[2]=$'\u2605\u20\u2605'
+# stars[2/5]=$'\u2605\u20\u2605\u20\u2606\u20\u2606\u20\u2606'
+# stars[3/5]=$'\u2605\u20\u2605\u20\u2605\u20\u2606\u20\u2606'
+# stars[4/5]=$'\u2605\u20\u2605\u20\u2605\u20\u2605\u20\u2606'
+# stars[5/5]=$'\u2605\u20\u2605\u20\u2605\u20\u2605\u20\u2605'
 
 get_random_number() {
-  if [[ $1 ]]; then
-    print $(( RANDOM % $1 ))
+  local variable_name output
+  variable_name=$1
+
+  if [[ $2 ]]; then
+    output=$(( $RANDOM % $2 ))
   else
     # magick number from zsh(1) manpage
-    print $(( 1.0 * RANDOM / 32767 ))
+    output=$(( 1.0 * $RANDOM / 32767 ))
   fi
+  : ${(P)${variable_name}::=${output}}
 }
 
 get_gem_type() {
+  local variable_name output
   local number_of_gem_types random_index
+  variable_name=$1
   # means returns number of items in array
   # (k) means return keys as an array
   number_of_gem_types=${#${(k)gems}}
   # (k) means return keys as an array
-  r=$(get_random_number $number_of_gem_types)
-  : echo $(( r % number_of_gem_types +1 ))
-  print ${${(k)gems}[(( r % number_of_gem_types + 1 ))]}
+  get_random_number random_index $number_of_gem_types
+  output=${${(k)gems}[(( random_index % number_of_gem_types + 1 ))]}
+  : ${(P)${variable_name}::=${output}}
 }
 
 get_gem_name() {
-  local number_of_gems_in_type
+  local variable_name output
+  variable_name=$1
+  local gem_type number_of_gems_in_type
+  gem_type=$2
   # (ps: :) means split on space, return as array
   # means returns number of items in array
   number_of_gems_in_type=${#${(ps: :)gems[$gem_type]}}
   # print ${${gems[(( RANDOM % number_of_gem_in_type + 1 ))]}}
-  print ${${(ps: :)gems[$gem_type]}[(( RANDOM % number_of_gems_in_type + 1))]}
-
+  get_random_number gem_selection $(( number_of_gems_in_type-1 ))
+  output=${${(ps: :)gems[$gem_type]}[(( gem_selection+2 ))]}
+  : ${(P)${variable_name}::=${output}}
 }
 
+get_gem_stars() {
+  # i know, a lot of boilerplate.
+  # thus is the price of avoiding a subshell
+  # don't need to do these things in a _real_ language
+  # or i could just - *GASP* - use **GLOBALS**
+  local variable_name output
+  variable_name=$1
+
+  output=${${(ps: :)gems[$gem_type]}[1]}
+
+  : ${(P)${variable_name}::=${output}}
+}
 rolls=0
 
 # set PRNG seed, if passed
@@ -53,12 +75,26 @@ rolls=0
 #  RANDOM=$1
 #fi
 
-while [[ rolls -lt 10 ]]; do
-  print RAND=$(get_random_number)
-  gem_type=$(get_gem_type)
-  gem=$(get_gem_name $gem_type)
+## debugging function; safe to ignore
+# inc() {
+#   local variable_name output
+#   variable_name=$1
+#   output=${(P)${variable_name}}
+#   : $(( output++ ))
+#   : ${(P)${variable_name}::=${output}}
+# }
 
-  echo "you found a $stars[$gem_type]  $gem"
+
+while [[ rolls -lt 10 ]]; do
+  # echo -n "internal: "
+  # inc _internal
+  # print $_internal
+  # print RAND=$(get_random_number $RANDOM)
+  # echo 3x: $RANDOM $RANDOM $RANDOM
+  get_gem_type gem_type
+  get_gem_name gem $gem_type
+  get_gem_stars stars $gem_type
+  echo "you found a $stars  $gem"
 
   # increment rolls
   : $(( rolls++ ))
