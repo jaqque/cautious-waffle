2d0f0b6447ef795c5b15bfe07430e9bfd0b10df9
SadieDragon <53710912+SadieDragon@users.noreply.github.com>
2024-02-19 20:42:57 -0600

    correct the inconsistent egg counts
    (When did it change?)

--- less basic egg.py
+++ less basic egg.py
@@ -5,13 +5,13 @@ from random import randint
 # Rather than storing an array, we can remember how many of each egg there are.
 # There are 6 white eggs, 4 brown eggs, and 2 blue eggs.
 # So if we pick any number between 1 and 6, we get a white egg.
-# Any number between 7 and 9, we'll get a brown egg.
-# Any number between 10 and 12, we'll get a blue egg.
+# Any number between 7 and 10, we'll get a brown egg.
+# Any number between 11 and 12, we'll get a blue egg.
 
 # This can be rewritten as:
 # 1  = x and x <= 6  white
-# 7  = x and x <= 9  brown
-# 10 = x and x <= 12 blue
+# 7  = x and x <= 10  brown
+# 11 = x and x <= 12 blue
 
 # There are other ways to rewrite this, but let's use this syntax first
 # (Actually 2023 me, you wrote that terribly)
@@ -25,9 +25,9 @@ chosen_number = randint(1, 12)
 egg = ''
 if ((chosen_number == 1) and (chosen_number <= 6)):
   egg = 'white'
-elif ((chosen_number == 7) and (chosen_number <= 9)):
+elif ((chosen_number == 7) and (chosen_number <= 10)):
   egg = 'brown'
-elif ((chosen_number == 10) and (chosen_number <= 12)):
+elif ((chosen_number == 11) and (chosen_number <= 12)):
   egg = 'blue'
 else:
   egg = 'Call the farmer!'
