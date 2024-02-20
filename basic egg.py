b6cc64087929562e329b68dab1c3d40cb71db8c5
SadieDragon <53710912+SadieDragon@users.noreply.github.com>
2024-02-19 20:20:40 -0600

    re-explain the basic implementation
    The original Ruby example was ok for illustrative ideas, but.. Not very good in practice. This is more of a balance. "You *could* write an array for this.. and pick a random index to pick a random egg..."

---
+++ basic egg.py
@@ -0,0 +1,19 @@
+from random import randint
+
+# Let's say we have a basket of a dozen eggs.
+# There are 6 white eggs, 4 brown eggs, and 2 blue eggs.
+# We want to pick a random egg from that basket.
+
+# So let's draw that basket as an array.
+basket = ['white', 'white', 'white', 'white', 'white', 'white', 'brown',
+          'brown', 'brown', 'brown', 'blue', 'blue']
+
+# Then, let's pick a random number.
+chosen_number = randint(1, 12)
+
+# Our egg that we've chosen will be at that number, minus 1
+# (because lists are indexed starting at 0)
+chosen_egg = basket[chosen_number - 1]
+
+print(f'The number you chose was {chosen_number}. This means you picked a '
+      f'{chosen_egg} egg.')
